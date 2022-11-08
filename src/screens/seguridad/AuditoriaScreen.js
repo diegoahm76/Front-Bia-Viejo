72d4bae43829f99c0d7fb6a33b02b2dc45a74011
Junior Pacheco <junior.pacheco.356@gmail.com>
2022-11-08 08:31:48 -0500

    cambios auditoria quitando paginacion

--- src/screens/seguridad/AuditoriaScreen.js
+++ src/screens/seguridad/AuditoriaScreen.js
@@ -70,9 +70,6 @@ const AuditoriaScreen = () => {
   const [auditorias, setAuditorias] = useState([]);
   const [subsistemasOptions, setSubsistemasOptions] = useState([]);
   const [tipoDocumentoOptions, setTipoDocumentoOptions] = useState([]);
-  const [page, setPage] = useState(1);
-  const [urlBasePagination, setUrlBasePagination] = useState("");
-  const [maxPage, setMaxPage] = useState(1);
   const [formValues, setFormValues] = useState({
     fechaIni: "",
     fechaEnd: "",
@@ -114,14 +111,7 @@ const AuditoriaScreen = () => {
       );
       console.log("data response auditorias, success", dataAuditorias);
       const dataSend = dataAuditorias.auditorias ?? [];
-      if (dataAuditorias.auditorias) {
-        setUrlBasePagination(queryParamsUrl);
-        setMaxPage(dataAuditorias.pages);
-      } else {
-        setUrlBasePagination("");
-      }
       setAuditorias(dataSend);
-      setPage(1);
     } catch (err) {
       console.log(err);
     }
@@ -145,44 +135,6 @@ const AuditoriaScreen = () => {
     };
     getInfo();
   }, []);
-
-  const getPageRequest = async (urlBase) => {
-    const accessToken = getTokenAccessLocalStorage();
-    const config = getConfigAuthBearer(accessToken);
-    const { data: dataAuditorias } = await clienteAxios.get(urlBase, config);
-    console.log(dataAuditorias);
-    setAuditorias(dataAuditorias.auditorias);
-  };
-
-  const nextPage = async () => {
-    const valueNextPage = page + 1;
-    if (valueNextPage <= maxPage) {
-      const completeUrl = urlBasePagination + `&page=${valueNextPage}`;
-      await getPageRequest(completeUrl);
-      setPage(valueNextPage);
-    }
-  };
-
-  const previousPage = async () => {
-    const valuePreviousPage = page - 1;
-    if (valuePreviousPage >= 1) {
-      const completeUrl = urlBasePagination + `&page=${valuePreviousPage}`;
-      await getPageRequest(completeUrl);
-      setPage(valuePreviousPage);
-    }
-  };
-
-  const lastPage = async () => {
-    const completeUrl = urlBasePagination + `&page=${maxPage}`;
-    await getPageRequest(completeUrl);
-    setPage(maxPage);
-  };
-
-  const firstPage = async () => {
-    const completeUrl = urlBasePagination + `&page=${1}`;
-    await getPageRequest(completeUrl);
-    setPage(1);
-  };
 
   return (
     <div className="row min-vh-100">
@@ -357,49 +309,9 @@ const AuditoriaScreen = () => {
                   columnDefs={columDefs}
                   rowData={auditorias}
                   defaultColDef={defaultColDef}
+                  paginationPageSize={10}
                 ></AgGridReact>
               </div>
-            </div>
-            <div className="d-flex justify-content-center gap-2">
-              <button
-                type="button"
-                className="btn btn-tablas bg-gradient-primary text-capitalize mt-3"
-                disabled={urlBasePagination === ""}
-                onClick={firstPage}
-              >
-                {"<<"}
-              </button>
-              <button
-                type="button"
-                className="btn btn-tablas bg-gradient-primary text-capitalize mt-3"
-                disabled={urlBasePagination === ""}
-                onClick={previousPage}
-              >
-                {"<"}
-              </button>
-              <button
-                type="button"
-                className="btn btn-tablas bg-gradient-primary text-capitalize mt-3"
-                disabled
-              >
-                {page}
-              </button>
-              <button
-                type="button"
-                className="btn btn-tablas bg-gradient-primary text-capitalize mt-3"
-                disabled={urlBasePagination === ""}
-                onClick={nextPage}
-              >
-                {">"}
-              </button>
-              <button
-                type="button"
-                className="btn btn-tablas bg-gradient-primary text-capitalize mt-3"
-                disabled={urlBasePagination === ""}
-                onClick={lastPage}
-              >
-                {">>"}
-              </button>
             </div>
           </form>
         </div>
