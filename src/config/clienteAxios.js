97215194e919712c406aa8a7d4968ba3e4e51d07
angelica leon quiroga <angelica.leon.quiroga@gmail.com>
2022-11-11 18:46:36 -0500

    crear, mostrar y eliminar de bodega

--- src/config/clienteAxios.js
+++ src/config/clienteAxios.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const clienteAxios = axios.create({
   baseURL: "https://web-production-e5dc.up.railway.app/api/",
+  
 });
 
 //https://web-production-4514.up.railway.app/api/
