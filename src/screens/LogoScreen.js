a791045d7bdc68db52a7ad4bbd51083468b861fe
Jesus Cruz <jesus.cruz@campusvirtual.aunarvillavicencio.edu.co>
2022-11-14 23:14:14 -0500

    Ultimo cambio slider

--- src/screens/LogoScreen.js
+++ src/screens/LogoScreen.js
@@ -67,143 +67,144 @@ function LogoScreen() {
           </div>
         </div>
       </div>
-      <div
-        className="container mt-4 align-items-center"
-        style={{ overflow: "hidden", width: "500px"}}
-      >
+      <div className="container position-relative w-50">
         <div
-          id="carouselExampleCaptions"
-          className="carousel slide"
-          data-bs-ride="carousel"
+          className="container mt-6 align-items-center"
+          style={{ overflow: "hidden", width: "400px" }}
         >
-          <div className="carousel-inner">
-            <div className="carousel-item active" data-bs-interval="10000">
-              <img
-                src={almacen}
-                className="d-block w-100 rounded rounded-circle aspect-ratio"
-                alt="..."
-              />
-              <div className="carousel-caption d-none d-md-block">
-                <h5 className="text-white">Almacen</h5>
+          <div
+            id="carouselExampleCaptions"
+            className="carousel slide"
+            data-bs-ride="carousel"
+          >
+            <div className="carousel-inner">
+              <div className="carousel-item active" data-bs-interval="10000">
+                <img
+                  src={almacen}
+                  className="d-block w-100 aspect-ratio"
+                  alt="..."
+                />
+                <div className="carousel-caption d-none d-md-block">
+                  <h5 className="text-white">Almacen</h5>
+                </div>
               </div>
-            </div>
-            <div className="carousel-item" data-bs-interval="10000">
-              <img
-                src={analiticaDeDatos}
-                className="d-block w-100 rounded rounded-circle aspect-ratio"
-                alt="..."
-              />
-              <div className="carousel-caption d-none d-md-block">
-                <h5 className="text-white">Analitica de datos</h5>
+              <div className="carousel-item" data-bs-interval="10000">
+                <img
+                  src={analiticaDeDatos}
+                  className="d-block w-100 aspect-ratio"
+                  alt="..."
+                />
+                <div className="carousel-caption d-none d-md-block">
+                  <h5 className="text-white">Analitica de datos</h5>
+                </div>
               </div>
-            </div>
-            <div className="carousel-item" data-bs-interval="10000">
-              <img
-                src={estacionesHidrometeorologicas}
-                className="d-block w-100 rounded rounded-circle aspect-ratio"
-                alt="..."
-              />
-              <div className="carousel-caption d-none d-md-block">
-                <h5 className="text-white">Estaciones hidrometerologicas</h5>
+              <div className="carousel-item" data-bs-interval="10000">
+                <img
+                  src={estacionesHidrometeorologicas}
+                  className="d-block w-100 aspect-ratio"
+                  alt="..."
+                />
+                <div className="carousel-caption d-none d-md-block">
+                  <h5 className="text-white">Estaciones hidrometerologicas</h5>
+                </div>
               </div>
-            </div>
-            <div className="carousel-item" data-bs-interval="10000">
-              <img
-                src={gestionDeViveros}
-                className="d-block w-100 rounded rounded-circle aspect-ratio"
-                alt="..."
-              />
-              <div className="carousel-caption d-none d-md-block">
-                <h5 className="text-white">Gestion de viveros</h5>
+              <div className="carousel-item" data-bs-interval="10000">
+                <img
+                  src={gestionDeViveros}
+                  className="d-block w-100 aspect-ratio"
+                  alt="..."
+                />
+                <div className="carousel-caption d-none d-md-block">
+                  <h5 className="text-white">Gestion de viveros</h5>
+                </div>
               </div>
-            </div>
-            <div className="carousel-item" data-bs-interval="10000">
-              <img
-                src={gestionDocumental}
-                className="d-block w-100 rounded rounded-circle aspect-ratio"
-                alt="..."
-              />
-              <div className="carousel-caption d-none d-md-block">
-                <h5 className="text-white">Gestion documental</h5>
+              <div className="carousel-item" data-bs-interval="10000">
+                <img
+                  src={gestionDocumental}
+                  className="d-block w-100 aspect-ratio"
+                  alt="..."
+                />
+                <div className="carousel-caption d-none d-md-block">
+                  <h5 className="text-white">Gestion documental</h5>
+                </div>
               </div>
-            </div>
-            <div className="carousel-item" data-bs-interval="10000">
-              <img
-                src={mandoControlAmbiental}
-                className="d-block w-100 rounded rounded-circle aspect-ratio"
-                alt="..."
-              />
-              <div className="carousel-caption d-none d-md-block">
-                <h5 className="text-white">Mando de control ambiental</h5>
+              <div className="carousel-item" data-bs-interval="10000">
+                <img
+                  src={mandoControlAmbiental}
+                  className="d-block w-100 aspect-ratio"
+                  alt="..."
+                />
+                <div className="carousel-caption d-none d-md-block">
+                  <h5 className="text-white">Mando de control ambiental</h5>
+                </div>
               </div>
-            </div>
-            <div className="carousel-item" data-bs-interval="10000">
-              <img
-                src={recursoHidrico}
-                className="d-block w-100 rounded rounded-circle aspect-ratio"
-                alt="..."
-              />
-              <div className="carousel-caption d-none d-md-block">
-                <h5 className="text-white">Recurso hidrico</h5>
+              <div className="carousel-item" data-bs-interval="10000">
+                <img
+                  src={recursoHidrico}
+                  className="d-block w-100 aspect-ratio"
+                  alt="..."
+                />
+                <div className="carousel-caption d-none d-md-block">
+                  <h5 className="text-white">Recurso hidrico</h5>
+                </div>
               </div>
-            </div>
-            <div className="carousel-item" data-bs-interval="10000">
-              <img
-                src={rentas}
-                className="d-block w-100 rounded rounded-circle aspect-ratio"
-                alt="..."
-              />
-              <div className="carousel-caption d-none d-md-block">
-                <h5 className="text-white">Rentas</h5>
+              <div className="carousel-item" data-bs-interval="10000">
+                <img
+                  src={rentas}
+                  className="d-block w-100 aspect-ratio"
+                  alt="..."
+                />
+                <div className="carousel-caption d-none d-md-block">
+                  <h5 className="text-white">Rentas</h5>
+                </div>
               </div>
-            </div>
-            <div className="carousel-item" data-bs-interval="10000">
-              <img
-                src={seguimientoPlanes}
-                className="d-block w-100 rounded rounded-circle aspect-ratio"
-                alt="..."
-              />
-              <div className="carousel-caption d-none d-md-block">
-                <h5 className="text-white">Seguimiento a planes</h5>
+              <div className="carousel-item" data-bs-interval="10000">
+                <img
+                  src={seguimientoPlanes}
+                  className="d-block w-100 aspect-ratio"
+                  alt="..."
+                />
+                <div className="carousel-caption d-none d-md-block">
+                  <h5 className="text-white">Seguimiento a planes</h5>
+                </div>
               </div>
-            </div>
-            <div className="carousel-item" data-bs-interval="10000">
-              <img
-                src={tramites}
-                className="d-block w-100 rounded rounded-circle aspect-ratio"
-                alt="..."
-              />
-              <div className="carousel-caption d-none d-md-block">
-                <h5 className="text-white">Tramites</h5>
+              <div className="carousel-item" data-bs-interval="10000">
+                <img
+                  src={tramites}
+                  className="d-block w-100 aspect-ratio"
+                  alt="..."
+                />
+                <div className="carousel-caption d-none d-md-block">
+                  <h5 className="text-white">Tramites</h5>
+                </div>
               </div>
             </div>
           </div>
-
-          <button
-            className="carousel-control-prev"
-            type="button"
-            data-bs-target="#carouselExampleCaptions"
-            data-bs-slide="prev"
-          >
-            <span
-              className="carousel-control-prev-icon"
-              aria-hidden="true"
-            ></span>
-            <span className="visually-hidden">Previous</span>
-          </button>
-          <button
-            className="carousel-control-next"
-            type="button"
-            data-bs-target="#carouselExampleCaptions"
-            data-bs-slide="next"
-          >
-            <span
-              className="carousel-control-next-icon"
-              aria-hidden="true"
-            ></span>
-            <span className="visually-hidden">Next</span>
-          </button>
         </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
       </div>
     </div>
   );
