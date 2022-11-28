6b0cdf09cb1818f6dc3796ab220e327d2aff07c4
GermanDev7 <GermanDev7@GMAIL.COM>
2022-11-28 16:14:36 -0500

    adelanto login

--- src/components/UserProtectedRoutes.tsx
+++ src/components/UserProtectedRoutes.tsx
@@ -1,11 +1,14 @@
-import { useSelector } from "react-redux";
+
+import React from 'react'
 import { Navigate, Outlet } from "react-router-dom/dist";
+import { useAppSelector } from "../store/store";
 
 const UserProtectedRoutes = () => {
-  const userInfo = useSelector((state) => state.user.user);
+  const userInfo = useAppSelector((state ) => state.login.initialState.user_info);
 
   if (!userInfo.is_superuser) {
     return <Outlet />;
+    
   }
   return <Navigate to={"/dashboard"} />;
 };
