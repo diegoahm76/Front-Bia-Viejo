63d9dbfa4241b49d08a18cd56385b6766b38abdd
AncizarTorres <ancizar_19_99@hotmail.com>
2023-02-01 17:41:28 -0500

    ajust Organigram Dialog

--- src/components/ModelOrganigram/OrganigramVisual.tsx
+++ src/components/ModelOrganigram/OrganigramVisual.tsx
@@ -15,16 +15,28 @@ export default function OrganigramVisual() {
     const stylesTree = { padding: '5px', border: '1px solid black', borderRadius: '8px', display: 'inline-block' };
     const stylesTreeNode = { padding: '5px', border: '1px solid black', borderRadius: '8px', display: 'inline-block' };
 
+    function StyleColor(type: string) {
+        switch (type) {
+            case 'LI':
+                return '#4BCF76';
+            case 'AS':
+                return '#EB4949';
+            case 'AP':
+                return '#2E64FE';
+            default:
+                return '#6bb22b';
+        }
+    }
     function CreateMoldOrganigram(data: ISon[]) {
         return data.map((item: any) => {
             return (
-                <TreeNode label={<div style={{ ...stylesTree, background: `${item.cod_agrupacion_documental ? '#47ACA6' : item.cod_agrupacion_documental === 'SUB' ? '#3aaad4' : '#3aaad4'}`, color: '#fff' }}>{item.nombre} </div>}>
+                <TreeNode label={<div style={{ ...stylesTree, background: StyleColor(item.cod_tipo_unidad), color: '#fff' }}>{item.nombre} </div>}>
                     {item.hijos.length > 0 ? CreateMoldOrganigram(item.hijos) : null}
                 </TreeNode>
             )
         })
     }
-
+    console.log(moldOrganigram, 'moldOrganigram')
     return (
         <>
             <Tree
