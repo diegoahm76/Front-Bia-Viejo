fc8e5d2612946f14bb7ee4dafc18e8b69ac6982e
Elver Rodriguez <adelson9602@gmail.com>
2023-02-04 12:18:38 -0500

    Se esta dando estilos al organigrama

--- src/components/ModelOrganigram/OrganigramVisual.tsx
+++ src/components/ModelOrganigram/OrganigramVisual.tsx
@@ -4,262 +4,314 @@ import { Button } from 'react-bootstrap';
 import OrganizationChart from '@dabeng/react-orgchart';
 // import { ISon } from '../../Interfaces/Organigrama';
 import { useAppSelector } from '../../store/hooks/hooks';
+import clienteAxios from '../../config/clienteAxios';
 
+// Styles
+import '../../css/organigrama_styles.css';
+import MyNode from './CustomNode';
+
+let dataView = {};
 export default function OrganigramVisual() {
   // Redux State Extraction
-  const { moldOrganigram } = useAppSelector((state) => state.organigram);
-  const ds = {
-    id: 464,
-    id_organigrama: 20,
-    id_nivel_organigrama: 66,
-    codigo: '1',
-    cod_tipo_unidad: 'LI',
-    cod_agrupacion_documental: 'SEC',
-    unidad_raiz: true,
-    id_unidad_org_padre: null,
-    orden_nivel: 1,
-    title: 'consejo directivo ',
-    children: [
-      {
-        id: 465,
+  const { organigramCurrent } = useAppSelector((state) => state.organigram);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getJerarquia = async () => {
+    setIsLoading(true);
+    try {
+      const { data } = await clienteAxios.get(
+        `almacen/organigrama/unidades/get-jerarquia/${organigramCurrent.id_organigrama}/`
+      );
+      dataView = {
         id_organigrama: 20,
-        id_nivel_organigrama: 67,
-        codigo: '2',
+        id_nivel_organigrama: 66,
+        codigo: '1',
         cod_tipo_unidad: 'LI',
-        cod_agrupacion_documental: 'SUB',
-        unidad_raiz: false,
-        id_unidad_org_padre: 464,
-        orden_nivel: 2,
-        title: 'jefatura',
+        cod_agrupacion_documental: 'SEC',
+        unidad_raiz: true,
+        id_unidad_org_padre: null,
+        orden_nivel: 1,
+        title: 'consejo directivo ',
+        id: 464,
+        className: 'top-level',
         children: [
           {
-            id: 466,
             id_organigrama: 20,
-            id_nivel_organigrama: 68,
-            codigo: '5',
-            cod_tipo_unidad: 'AP',
-            cod_agrupacion_documental: null,
-            unidad_raiz: false,
-            id_unidad_org_padre: 465,
-            orden_nivel: 3,
-            title: 'Gerencia general',
-            children: []
-          },
-          {
-            id: 467,
-            id_organigrama: 20,
-            id_nivel_organigrama: 68,
-            codigo: '3',
-            cod_tipo_unidad: 'AS',
-            cod_agrupacion_documental: null,
-            unidad_raiz: false,
-            id_unidad_org_padre: 465,
-            orden_nivel: 3,
-            title: 'organo de control institucional ',
-            children: []
-          },
-          {
-            id: 468,
-            id_organigrama: 20,
-            id_nivel_organigrama: 68,
-            codigo: '4',
-            cod_tipo_unidad: 'AS',
-            cod_agrupacion_documental: null,
-            unidad_raiz: false,
-            id_unidad_org_padre: 465,
-            orden_nivel: 3,
-            title: 'tribunal nacional de resolución ',
-            children: []
-          },
-          {
-            id: 469,
-            id_organigrama: 20,
-            id_nivel_organigrama: 69,
-            codigo: '8 ',
+            id_nivel_organigrama: 67,
+            codigo: '2',
             cod_tipo_unidad: 'LI',
             cod_agrupacion_documental: 'SUB',
             unidad_raiz: false,
-            id_unidad_org_padre: 465,
-            orden_nivel: 4,
-            title: 'oficina de asesoría jurídica ',
+            id_unidad_org_padre: 464,
+            orden_nivel: 2,
+            title: 'jefatura',
+            id: 465,
+            className: 'middle-level',
             children: [
               {
-                id: 473,
                 id_organigrama: 20,
-                id_nivel_organigrama: 70,
-                codigo: '18',
-                cod_tipo_unidad: 'AS',
+                id_nivel_organigrama: 68,
+                codigo: '5',
+                cod_tipo_unidad: 'AP',
                 cod_agrupacion_documental: null,
                 unidad_raiz: false,
-                id_unidad_org_padre: 469,
-                orden_nivel: 5,
-                title: 'administración local del agua ',
-                children: []
+                id_unidad_org_padre: 465,
+                orden_nivel: 3,
+                title: 'Gerencia general',
+                id: 466,
+                children: [],
+                relationship: '110',
+                className: 'bottom-level'
               },
               {
-                id: 475,
                 id_organigrama: 20,
-                id_nivel_organigrama: 70,
-                codigo: '17',
+                id_nivel_organigrama: 68,
+                codigo: '3',
                 cod_tipo_unidad: 'AS',
                 cod_agrupacion_documental: null,
                 unidad_raiz: false,
-                id_unidad_org_padre: 469,
-                orden_nivel: 5,
-                title: 'autoridad administrativa',
-                children: []
+                id_unidad_org_padre: 465,
+                orden_nivel: 3,
+                title: 'organo de control institucional ',
+                id: 467,
+                children: [],
+                relationship: '110',
+                className: 'bottom-level'
+              },
+              {
+                id_organigrama: 20,
+                id_nivel_organigrama: 68,
+                codigo: '4',
+                cod_tipo_unidad: 'AS',
+                cod_agrupacion_documental: null,
+                unidad_raiz: false,
+                id_unidad_org_padre: 465,
+                orden_nivel: 3,
+                title: 'tribunal nacional de resolución ',
+                id: 468,
+                children: [],
+                relationship: '110',
+                className: 'bottom-level'
+              },
+              {
+                id_organigrama: 20,
+                id_nivel_organigrama: 69,
+                codigo: '8 ',
+                cod_tipo_unidad: 'LI',
+                cod_agrupacion_documental: 'SUB',
+                unidad_raiz: false,
+                id_unidad_org_padre: 465,
+                orden_nivel: 4,
+                title: 'oficina de asesoría jurídica ',
+                id: 469,
+                children: [
+                  {
+                    id_organigrama: 20,
+                    id_nivel_organigrama: 70,
+                    codigo: '18',
+                    cod_tipo_unidad: 'AS',
+                    cod_agrupacion_documental: null,
+                    unidad_raiz: false,
+                    id_unidad_org_padre: 469,
+                    orden_nivel: 5,
+                    title: 'administración local del agua ',
+                    id: 473,
+                    children: [],
+                    relationship: '110'
+                  },
+                  {
+                    id_organigrama: 20,
+                    id_nivel_organigrama: 70,
+                    codigo: '17',
+                    cod_tipo_unidad: 'AS',
+                    cod_agrupacion_documental: null,
+                    unidad_raiz: false,
+                    id_unidad_org_padre: 469,
+                    orden_nivel: 5,
+                    title: 'autoridad administrativa',
+                    id: 475,
+                    children: [],
+                    relationship: '110'
+                  }
+                ],
+                relationship: '111',
+                className: 'bottom-level'
+              },
+              {
+                id_organigrama: 20,
+                id_nivel_organigrama: 69,
+                codigo: '7',
+                cod_tipo_unidad: 'LI',
+                cod_agrupacion_documental: 'SUB',
+                unidad_raiz: false,
+                id_unidad_org_padre: 465,
+                orden_nivel: 4,
+                title: 'oficina de planeamiento y presupuesto ',
+                id: 470,
+                children: [
+                  {
+                    id_organigrama: 20,
+                    id_nivel_organigrama: 70,
+                    codigo: '16',
+                    cod_tipo_unidad: 'AP',
+                    cod_agrupacion_documental: null,
+                    unidad_raiz: false,
+                    id_unidad_org_padre: 470,
+                    orden_nivel: 5,
+                    title: 'unidad de cooperación ',
+                    id: 476,
+                    children: [],
+                    relationship: '110'
+                  },
+                  {
+                    id_organigrama: 20,
+                    id_nivel_organigrama: 70,
+                    codigo: '15',
+                    cod_tipo_unidad: 'AS',
+                    cod_agrupacion_documental: null,
+                    unidad_raiz: false,
+                    id_unidad_org_padre: 470,
+                    orden_nivel: 5,
+                    title: 'unidad de presupuesto ',
+                    id: 477,
+                    children: [],
+                    relationship: '110'
+                  }
+                ],
+                relationship: '111',
+                className: 'bottom-level'
+              },
+              {
+                id_organigrama: 20,
+                id_nivel_organigrama: 69,
+                codigo: '6',
+                cod_tipo_unidad: 'LI',
+                cod_agrupacion_documental: 'SUB',
+                unidad_raiz: false,
+                id_unidad_org_padre: 465,
+                orden_nivel: 4,
+                title: 'oficina de administración ',
+                id: 471,
+                children: [
+                  {
+                    id_organigrama: 20,
+                    id_nivel_organigrama: 70,
+                    codigo: '10',
+                    cod_tipo_unidad: 'AS',
+                    cod_agrupacion_documental: null,
+                    unidad_raiz: false,
+                    id_unidad_org_padre: 471,
+                    orden_nivel: 5,
+                    title: 'unidad de contabilidad y tesorería  ',
+                    id: 472,
+                    children: [],
+                    relationship: '110'
+                  },
+                  {
+                    id_organigrama: 20,
+                    id_nivel_organigrama: 70,
+                    codigo: '9',
+                    cod_tipo_unidad: 'AS',
+                    cod_agrupacion_documental: null,
+                    unidad_raiz: false,
+                    id_unidad_org_padre: 471,
+                    orden_nivel: 5,
+                    title: 'unidad de abastecimiento y patrimonio ',
+                    id: 474,
+                    children: [],
+                    relationship: '110'
+                  },
+                  {
+                    id_organigrama: 20,
+                    id_nivel_organigrama: 70,
+                    codigo: '14',
+                    cod_tipo_unidad: 'AS',
+                    cod_agrupacion_documental: null,
+                    unidad_raiz: false,
+                    id_unidad_org_padre: 471,
+                    orden_nivel: 5,
+                    title: 'unidad planeamiento y modernización ',
+                    id: 478,
+                    children: [],
+                    relationship: '110'
+                  },
+                  {
+                    id_organigrama: 20,
+                    id_nivel_organigrama: 70,
+                    codigo: '13',
+                    cod_tipo_unidad: 'AP',
+                    cod_agrupacion_documental: null,
+                    unidad_raiz: false,
+                    id_unidad_org_padre: 471,
+                    orden_nivel: 5,
+                    title: 'unidad de recursos humanos ',
+                    id: 479,
+                    children: [],
+                    relationship: '110'
+                  },
+                  {
+                    id_organigrama: 20,
+                    id_nivel_organigrama: 70,
+                    codigo: '12',
+                    cod_tipo_unidad: 'AP',
+                    cod_agrupacion_documental: null,
+                    unidad_raiz: false,
+                    id_unidad_org_padre: 471,
+                    orden_nivel: 5,
+                    title: 'unidad de ejecución coactiva',
+                    id: 480,
+                    children: [],
+                    relationship: '110'
+                  },
+                  {
+                    id_organigrama: 20,
+                    id_nivel_organigrama: 70,
+                    codigo: '11',
+                    cod_tipo_unidad: 'AP',
+                    cod_agrupacion_documental: null,
+                    unidad_raiz: false,
+                    id_unidad_org_padre: 471,
+                    orden_nivel: 5,
+                    title: 'unidad archivo y tramite documentario  ',
+                    id: 481,
+                    children: [],
+                    relationship: '110'
+                  },
+                  {
+                    id_organigrama: 20,
+                    id_nivel_organigrama: 70,
+                    codigo: '19',
+                    cod_tipo_unidad: 'AP',
+                    cod_agrupacion_documental: null,
+                    unidad_raiz: false,
+                    id_unidad_org_padre: 471,
+                    orden_nivel: 5,
+                    title: 'consejos de recursos hídricos de cuenca ',
+                    id: 482,
+                    children: [],
+                    relationship: '110'
+                  }
+                ],
+                relationship: '111',
+                className: 'bottom-level'
               }
-            ]
-          },
-          {
-            id: 470,
-            id_organigrama: 20,
-            id_nivel_organigrama: 69,
-            codigo: '7',
-            cod_tipo_unidad: 'LI',
-            cod_agrupacion_documental: 'SUB',
-            unidad_raiz: false,
-            id_unidad_org_padre: 465,
-            orden_nivel: 4,
-            title: 'oficina de planeamiento y presupuesto ',
-            children: [
-              {
-                id: 476,
-                id_organigrama: 20,
-                id_nivel_organigrama: 70,
-                codigo: '16',
-                cod_tipo_unidad: 'AP',
-                cod_agrupacion_documental: null,
-                unidad_raiz: false,
-                id_unidad_org_padre: 470,
-                orden_nivel: 5,
-                title: 'unidad de cooperación ',
-                children: []
-              },
-              {
-                id: 477,
-                id_organigrama: 20,
-                id_nivel_organigrama: 70,
-                codigo: '15',
-                cod_tipo_unidad: 'AS',
-                cod_agrupacion_documental: null,
-                unidad_raiz: false,
-                id_unidad_org_padre: 470,
-                orden_nivel: 5,
-                title: 'unidad de presupuesto ',
-                children: []
-              }
-            ]
-          },
-          {
-            id: 471,
-            id_organigrama: 20,
-            id_nivel_organigrama: 69,
-            codigo: '6',
-            cod_tipo_unidad: 'LI',
-            cod_agrupacion_documental: 'SUB',
-            unidad_raiz: false,
-            id_unidad_org_padre: 465,
-            orden_nivel: 4,
-            title: 'oficina de administración ',
-            children: [
-              {
-                id: 472,
-                id_organigrama: 20,
-                id_nivel_organigrama: 70,
-                codigo: '10',
-                cod_tipo_unidad: 'AS',
-                cod_agrupacion_documental: null,
-                unidad_raiz: false,
-                id_unidad_org_padre: 471,
-                orden_nivel: 5,
-                title: 'unidad de contabilidad y tesorería  ',
-                children: []
-              },
-              {
-                id: 474,
-                id_organigrama: 20,
-                id_nivel_organigrama: 70,
-                codigo: '9',
-                cod_tipo_unidad: 'AS',
-                cod_agrupacion_documental: null,
-                unidad_raiz: false,
-                id_unidad_org_padre: 471,
-                orden_nivel: 5,
-                title: 'unidad de abastecimiento y patrimonio ',
-                children: []
-              },
-              {
-                id: 478,
-                id_organigrama: 20,
-                id_nivel_organigrama: 70,
-                codigo: '14',
-                cod_tipo_unidad: 'AS',
-                cod_agrupacion_documental: null,
-                unidad_raiz: false,
-                id_unidad_org_padre: 471,
-                orden_nivel: 5,
-                title: 'unidad planeamiento y modernización ',
-                children: []
-              },
-              {
-                id: 479,
-                id_organigrama: 20,
-                id_nivel_organigrama: 70,
-                codigo: '13',
-                cod_tipo_unidad: 'AP',
-                cod_agrupacion_documental: null,
-                unidad_raiz: false,
-                id_unidad_org_padre: 471,
-                orden_nivel: 5,
-                title: 'unidad de recursos humanos ',
-                children: []
-              },
-              {
-                id: 480,
-                id_organigrama: 20,
-                id_nivel_organigrama: 70,
-                codigo: '12',
-                cod_tipo_unidad: 'AP',
-                cod_agrupacion_documental: null,
-                unidad_raiz: false,
-                id_unidad_org_padre: 471,
-                orden_nivel: 5,
-                title: 'unidad de ejecución coactiva',
-                children: []
-              },
-              {
-                id: 481,
-                id_organigrama: 20,
-                id_nivel_organigrama: 70,
-                codigo: '11',
-                cod_tipo_unidad: 'AP',
-                cod_agrupacion_documental: null,
-                unidad_raiz: false,
-                id_unidad_org_padre: 471,
-                orden_nivel: 5,
-                title: 'unidad archivo y tramite documentario  ',
-                children: []
-              },
-              {
-                id: 482,
-                id_organigrama: 20,
-                id_nivel_organigrama: 70,
-                codigo: '19',
-                cod_tipo_unidad: 'AP',
-                cod_agrupacion_documental: null,
-                unidad_raiz: false,
-                id_unidad_org_padre: 471,
-                orden_nivel: 5,
-                title: 'consejos de recursos hídricos de cuenca ',
-                children: []
-              }
-            ]
+            ],
+            relationship: '101'
           }
-        ]
-      }
-    ]
+        ],
+        relationship: '001'
+      };
+      // console.log(dataView);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
   };
+
+  useEffect(() => {
+    getJerarquia();
+  }, []);
 
   //   function StyleColor(type: string) {
   //     switch (type) {
@@ -284,7 +336,16 @@ export default function OrganigramVisual() {
       <Button style={{ color: '#fff' }} disabled variant='success'>
         Unidad de Apoyo
       </Button>{' '}
-      <OrganizationChart datasource={ds} direction='b2t' />
+      {isLoading ? (
+        'Cargango'
+      ) : (
+        <OrganizationChart
+          datasource={dataView}
+          zoom={true}
+          chartClass='myChart'
+          NodeTemplate={MyNode}
+        />
+      )}
     </>
   );
 }
